0126233f45b3983051206b2262bfceaba2b5523d
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-08-21 15:02:08 -0300

    create new collections

---
+++ src/api/my-page/controllers/my-page.ts
@@ -0,0 +1,7 @@
+/**
+ * my-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::my-page.my-page');
---
+++ src/api/my-page/routes/my-page.ts
@@ -0,0 +1,7 @@
+/**
+ * my-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::my-page.my-page');
